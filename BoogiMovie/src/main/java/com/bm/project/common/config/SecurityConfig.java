c0e909b276fb6f9c9d5444ebf57f41b4b0757122
peachpi3 <gkdpthf1@naver.com>
2025-12-23 17:54:26 +0900

    Feat. JWT 토큰 로직 작성

--- BoogiMovie/src/main/java/com/bm/project/common/config/SecurityConfig.java
+++ BoogiMovie/src/main/java/com/bm/project/common/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.bm.project.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,4 +19,9 @@ public class SecurityConfig {
 
         return http.build();
     }
+	
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- BoogiMovie/src/main/java/com/bm/project/jwt/model/dto/JwtToken.java
+++ BoogiMovie/src/main/java/com/bm/project/jwt/model/dto/JwtToken.java
@@ -8,7 +8,17 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class JwtToken {
-	private String grantType; // JWT에 대한 인증 타입, Bearer 인증 방식 사용 예정
-	private String accessToken;
-	private String refreshToken;
+	private String grantType; 		// JWT에 대한 인증 타입, Bearer 인증 방식 사용 예정
+	private String accessToken; 	// 액세스 토큰
+	private String refreshToken;    // 리프레시 토큰
+	
+	/* Bearer Token (무기명 토큰)
+	 * "이 토큰의 소유자를 인증하라"
+	 * 
+	 * - Oauth 2.0 프로토콜에서 사용되는 토큰이며, 인증된 사용자를 대신해 API에 접근할 수 있는 권한 부여
+	 * - JWT 토큰과 달리, 토큰 내에 사용자 정보를 포함시키지 않는다.
+	 * 
+	 * => JWT 토큰은 사용자 정보와 권한 정보를 포함시켜 전달하며, 
+	 *    Bearer 토큰은 인증된 사용자를 대신하여 API에 접근할 수 있는 권한을 부여한다.
+	 * */
 }
