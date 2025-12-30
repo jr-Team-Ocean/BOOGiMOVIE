38644c87162686df43585f0ddef225521820d6ee
TaeWoong <field56@naver.com>
2025-12-30 18:24:58 +0900

    12/30 ubook 작업

---
+++ BoogiMovie/src/main/java/com/bm/project/entity/Ubook.java
@@ -0,0 +1,62 @@
+package com.bm.project.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import com.bm.project.enums.CommonEnums;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="UBOOK")
+@Getter
+@Setter
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@DynamicInsert
+@DynamicUpdate
+public class Ubook {
+	
+	@Id
+	@Column(name = "PRODUCT_NO")
+	private Long productNo; // FK이면서 PK 역할
+
+	@Column(name="UBOOK_STATUS", length=6, nullable=false)
+	@Enumerated(EnumType.STRING)
+	private CommonEnums.UbookStatus ubookStatus;
+	
+	@Column(name="NBOOK_PRICE", nullable=false)
+	private Long nbookPrice;
+	
+	@Lob
+	@Column(name="UBOOK_INDEX", nullable=false)
+	private String ubookIndex;
+	
+	@Lob
+	@Column(name="AUTHOR_INTRO", nullable=false)
+	private String authorIntro;
+	
+	@MapsId // PK이면서 FK일 때 사용 
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="PRODUCT_NO")
+	private Product product;
+	 
+	
+}
