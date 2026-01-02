83725721ba0390fb0bff0cab23d805319ebd56f2
peachpi3 <gkdpthf1@naver.com>
2026-01-02 13:02:02 +0900

    Update. 홈화면 조회 끝!

--- BoogiMovie/src/main/java/com/bm/project/dto/HomeOrderDto.java
+++ BoogiMovie/src/main/java/com/bm/project/dto/HomeOrderDto.java
@@ -1,15 +1,15 @@
 package com.bm.project.dto;
 
 // 네이티브 쿼리 사용시 필요
-public interface HomeInterfaceDto {
-	
+public interface HomeOrderDto {
 	Long getProductNo();
     String getProductTitle();
     String getImgPath();
-    Long getLikeCount();
     
     // DB에서는 하나의 문자열로 넘어옴
     String getCreator(); 
     String getCompany();
-
+    
+    Long getOrderCount();
+    Long getTypeCode();
 }
